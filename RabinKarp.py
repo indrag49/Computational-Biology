0c56258bcd4f8f9a48acee98f7a951c437e6e549
indrag49 <indranilg49@gmail.com>
2018-01-25 23:05:48 +0530

    Add files via upload

---
+++ RabinKarp.py
@@ -0,0 +1,19 @@
+## Author: Indranil Ghosh, Jadavpur University, Physics Department
+
+# Rabin-Karp Algorithm
+
+def Hash(S):
+        a=0
+        for i in S: a=((ord(i)+a)%101)*256
+        return a/256
+
+def RK(S, T):
+        #S is the larger string and T is the pattern to be searched in S
+        H=Hash(T)
+        n=len(S)
+        l=len(T)
+        for i in range(n-l+1):
+                s=Hash(S[i:i+l])
+                if s==H:
+                        if S[i:i+l]==T[:l]: return i
+        return "No such substring pattern!!!"
