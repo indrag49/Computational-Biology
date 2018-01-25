1aca422b5cdbd577c5f66244675bf23c47e6a1f1
indrag49 <indranilg49@gmail.com>
2018-01-25 23:52:41 +0530

    Update RabinKarp.py

--- RabinKarp.py
+++ RabinKarp.py
@@ -2,6 +2,7 @@
 
 # Rabin-Karp Algorithm
 
+import pandas as pd
 def Hash(S):
         a=0
         for i in S: a=((ord(i)+a)%101)*256
@@ -15,5 +16,6 @@ def RK(S, T):
         for i in range(n-l+1):
                 s=Hash(S[i:i+l])
                 if s==H:
-                        if S[i:i+l]==T[:l]: return i
-        return "No such substring pattern!!!"
+                        if S[i:i+l]==T[:l]:
+                                return pd.DataFrame([S, i*"-"+T+(n-(i+l))*"-"], columns=["Alignment"])
+        return "No Alignment Found"
